5c34f97081715b084255f8c3a1caa43e1c8f9d27
pramod3225 <pramod3225@gmail.com>
2017-03-12 17:34:26 +0530

    modeified method add referral

--- routes/index.js
+++ routes/index.js
@@ -2,17 +2,13 @@ var express = require('express');
 var router = express.Router();
 var customerModel = require('../Models/customer-model.js');
 
-function raiseError(message, code){
-    var err = new Error(message);
-    err.status = code;
-    return next(err);
-}
+
 
 //1. addCustomer (For Adding A new Customer)
 router.post('/addCustomer', function (req, res,next) {
     var customerData = req.body;
     customerModel.create(customerData, function (err, result) {
-        if (err) {next(err); return; }
+        if (err) {return next(err);  }
         res.send(result)
     })
 
@@ -22,7 +18,7 @@ router.post('/addCustomer', function (req, res,next) {
 //2. getCustomerById (For fetching a customer details by ID)
 router.get('/getCustomerById/:id', function (req, res,next) {
     customerModel.find({"customer_id" : req.params.id}, function(err, customerData) {
-        if (err) {next(err); return; }
+        if (err) {return next(err);  }
         res.send(customerData);
     });
 
@@ -30,31 +26,43 @@ router.get('/getCustomerById/:id', function (req, res,next) {
 
 
 //3. addReferral (For Adding A Referral Under A Customer)
+
+
 router.post('/addReferral', function (req, res,next) {
     var customerData = req.body;
     if (customerData && customerData.referral_id){
-        customerModel.update({"customer_id" : customerData.referral_id},
-            {$inc: { payback: 30} , $currentDate: { lastUpdated: true }}, function(err, result){
-            if (err) {next(err); return; }
-            if (result.ok && !result.nModified){
-                raiseError('Referral Id is missing',404);
+        customerModel.findOneAndUpdate({"customer_id" : customerData.referral_id},
+            {$inc: { payback: 30} , $currentDate: { lastUpdated: true }},{new:true}, function(err, parCustomer){
+            if (err) {return next(err);  }
+            if (!parCustomer){
+                var err1 = new Error('Did not find Referral Id ');
+                err1.status = 404;
+                return next(err1);
+            }
+            if (parCustomer && parCustomer.referral_id) {
+                customerModel.update({"customer_id": parCustomer.referral_id, isAmbassador: true},
+                    {$inc: {payback: 10}, $currentDate: {lastUpdated: true}}, function (err) {
+                        if (err) {return next(err);  }
+                    })
             }
             customerModel.create(customerData, function (err, cresult) {
-                if (err) {next(err); return; }
+                if (err) {return next(err);  }
                 res.send(cresult)
             })
-
         })
+
     }
     else {
-        raiseError('Referral Id is missing',404);
+        var err = new Error('Referral Id is missing');
+        err.status = 404;
+        return next(err);
     }
 });
 
 //4. fetchAllChildren (Fetch All Children Under A Customer)
 router.get('/fetchAllChildren/:id', function (req, res,next) {
     customerModel.find({"referral_id" : req.params.id}, function(err, customerData) {
-        if (err) {next(err); return; }
+        if (err) {return next(err);  }
         res.send(customerData);
     });
 
@@ -87,7 +95,7 @@ router.post('/addAmbassador', function (req, res,next) {
     var customerData = req.body;
     customerData.isAmbassador = true;
     customerModel.create(customerData, function (err, result) {
-        if (err) {next(err); return; }
+        if (err) {return next(err);  }
         res.send(result)
     })
 
@@ -96,7 +104,7 @@ router.post('/addAmbassador', function (req, res,next) {
 //7. convertCustomerToAmbassador (A customer can be converted to an Ambassador. But will get additional 10% only on the customers added under him after the time he got converted.)
 router.post('/convertCustomerToAmbassador/:id', function (req, res,next) {
     customerModel.findOneAndUpdate({"customer_id" : req.params.id}, { isAmbassador: true },{new:true}, function(err, result){
-        if (err) {next(err); return; }
+        if (err) {return next(err);  }
         res.send(result)
     })
 
@@ -109,7 +117,7 @@ router.get('/fetchChildrenAtNthLevel/:n', function (req, res,next) {
     var refIds =[null];
     function getCustdataN(n,refIds){
         customerModel.find({"referral_id" : refIds}, function(err, customersData) {
-            if (err) {next(err); return; }
+            if (err) {return next(err);  }
             var custIdArray = customersData.map(function(item){return item.customer_id});
             if(--n>=0){
                 return getCustdataN(n,custIdArray)
@@ -122,17 +130,5 @@ router.get('/fetchChildrenAtNthLevel/:n', function (req, res,next) {
     getCustdataN(n , refIds);
 });
 
-
-
-
-
-//customer_id : 2
-//email : 'pramod3225@gmail.com'
-//referral_id :null
-//payback:0
-//isAmbassador : false
-//joiningDate : null
-//lastUpdated : null
-////});
 
 module.exports = router;
